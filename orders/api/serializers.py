c882682d006f80a9d49f0bf296c8e17de08f4e4f
Philipp Biebert <philipp.biebert@gmail.com>
2026-06-24 16:09:22 +0200

    fix(orders): update OrderDetailUpdateSerializer to include all fields and specify read-only fields

--- orders/api/serializers.py
+++ orders/api/serializers.py
@@ -61,7 +61,20 @@ class OrderDetailUpdateSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Order
-        fields = ["status"]
+        fields = "__all__"
+        read_only_fields = [
+            "id",
+            "customer_user",
+            "business_user",
+            "title",
+            "revisions",
+            "delivery_time_in_days",
+            "price",
+            "features",
+            "offer_type",
+            "created_at",
+            "updated_at",
+        ]
 
     def validate(self, attrs):
         """Ensure that only the 'status' field can be updated."""
